cb3028ed2ac2d89110a62af48477f65c5dfd6245
Sara Ferreira <saraywebst@gmail.com>
2024-12-20 00:34:16 -0300

    :construction: construction

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+config();
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // false
+    }),
+  ],
+
+  //FreightModule,
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
